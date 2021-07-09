e67ec7d1215b9dc93560f10905d794a69b398fe4
networkingcat <57919837+networkingcat@users.noreply.github.com>
2021-07-09 20:24:42 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,14 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-    def hello():
-        return "Hello from Germany"
+  def hello():
+    return "Hello from Germany"
 
 @app.route("/udacity")
-    def hello():
-        return "Udacity Capstone Project"
+  def hello():
+    return "Udacity Capstone Project"
 
 
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
