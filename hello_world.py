2fda6d82296a123ac223cb0933f0f3c2b952708f
networkingcat <57919837+networkingcat@users.noreply.github.com>
2021-07-09 18:53:15 +0200

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+    def hello():
+        return "Hello from Germany"
+
+@app.route("/udacity")
+    def hello():
+        return "Udacity Capstone Project"
+
+
+
+if __name__ == "__main__":
+    app.run()
